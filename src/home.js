c8bf909e9a4a3b3e8c102edb87b6f66c7ed039dd
CJskii <czareczarek3@gmail.com>
2022-11-13 23:37:28 +0000

    App init

--- src/home.js
+++ src/home.js
@@ -1,20 +1,55 @@
 export const App = {
   init: () => {
-    App.header();
+    const main = document.querySelector("body");
+    App.wrapper(main);
   },
-  wrapper: () => {
-    // create main container here
+  wrapper: (main) => {
+    // create main container
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("content");
+    main.appendChild(wrapper);
+    App.weather(wrapper);
+    App.forecast(wrapper);
   },
-  header: () => {
-    // create header
-  },
-  weather: () => {
+  weather: (wrapper) => {
     // create weather info container
+    const weather = document.createElement("div");
+    weather.classList.add("weather-container");
+    wrapper.appendChild(weather);
+    App.weatherInfo(weather);
+    App.forecastInfo(weather);
   },
-  forecast: () => {
+  forecast: (wrapper) => {
     // create forecast container
+    const forecast = document.createElement("div");
+    forecast.classList.add("forecast-container");
+    wrapper.appendChild(forecast);
+    App.forecastChange(forecast);
+    App.forecastContainer(forecast);
   },
-  // new module
+  weatherInfo: (weather) => {
+    const container = document.createElement("div");
+    container.classList.add("info-container");
+    weather.appendChild(container);
+  },
+  forecastInfo: (forecast) => {
+    const container = document.createElement("div");
+    container.classList.add("details-container");
+    forecast.appendChild(container);
+  },
+  forecastChange: (forecast) => {
+    const container = document.createElement("div");
+    container.classList.add("forecast-change");
+    forecast.appendChild(container);
+  },
+  forecastContainer: (forecast) => {
+    const container = document.createElement("div");
+    container.classList.add("forecast-container-daily");
+    forecast.appendChild(container);
+  },
+};
+
+const populate = {
   weatherItems: () => {
     // populate weather info
   },
