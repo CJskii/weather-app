527deeab1ac35c1a46ef572fa35b061947d5285d
CJskii <czareczarek3@gmail.com>
2022-11-13 15:57:08 +0000

    Initial commit

---
+++ src/home.js
@@ -0,0 +1,24 @@
+export const App = {
+  init: () => {
+    App.header();
+  },
+  wrapper: () => {
+    // create main container here
+  },
+  header: () => {
+    // create header
+  },
+  weather: () => {
+    // create weather info container
+  },
+  forecast: () => {
+    // create forecast container
+  },
+  // new module
+  weatherItems: () => {
+    // populate weather info
+  },
+  forecastItems: () => {
+    // populate forcast info
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import { App } from "./home.js";
+
+(function () {
+  // invoke app
+  App.init();
+  console.log("App invoked");
+})();
